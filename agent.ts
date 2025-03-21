308713feeb0b70d01327ba7b6d7ddf60322105dd
Zenati Mehdi <mehdi1.zenati@epitech.eu>
2025-03-22 00:27:09 +0100

    Adapted to the exercice (following langgraph documentation)

--- agent.ts
+++ agent.ts
@@ -1,37 +1,54 @@
+import 'dotenv/config'
 import { TavilySearchResults } from "@langchain/community/tools/tavily_search";
 import { ChatOpenAI } from "@langchain/openai";
-import { MemorySaver } from "@langchain/langgraph";
-import { HumanMessage } from "@langchain/core/messages";
-import { createReactAgent } from "@langchain/langgraph/prebuilt";
-import 'dotenv/config'
+import { HumanMessage, AIMessage } from "@langchain/core/messages";
+import { ToolNode } from "@langchain/langgraph/prebuilt";
+import { StateGraph, MessagesAnnotation } from "@langchain/langgraph";
+import RapportJSON from "./assets/rapport.json"
+const tools = [new TavilySearchResults({ maxResults: 3 })];
+const toolNode = new ToolNode(tools);
 
-// Define the tools for the agent to use
-const agentTools = [new TavilySearchResults({ maxResults: 3 })];
-const agentModel = new ChatOpenAI({ temperature: 0 });
+const model = new ChatOpenAI({
+  model: "gpt-4o-mini",
+  temperature: 0,
+}).bindTools(tools);
 
-// Initialize memory to persist state between graph runs
-const agentCheckpointer = new MemorySaver();
-const agent = createReactAgent({
-  llm: agentModel,
-  tools: agentTools,
-  checkpointSaver: agentCheckpointer,
+function shouldContinue({ messages }: typeof MessagesAnnotation.State) {
+  const lastMessage = messages[messages.length - 1] as AIMessage;
+
+  if (lastMessage.tool_calls?.length) {
+    return "tools";
+  }
+  return "__end__";
+}
+
+async function callModel(state: typeof MessagesAnnotation.State) {
+  const response = await model.invoke(state.messages);
+
+  return { messages: [response] };
+}
+
+const workflow = new StateGraph(MessagesAnnotation)
+  .addNode("agent", callModel)
+  .addEdge("__start__", "agent") 
+  .addNode("tools", toolNode)
+  .addEdge("tools", "agent")
+  .addConditionalEdges("agent", shouldContinue);
+
+const app = workflow.compile();
+const rapport = JSON.stringify(RapportJSON);
+
+const finalState = await app.invoke({
+  messages: [new HumanMessage("Take a look at this json :" + rapport)],
 });
+console.log(finalState.messages[finalState.messages.length - 1].content);
 
-// Now it's time to use!
-const agentFinalState = await agent.invoke(
-  { messages: [new HumanMessage("what is the current weather in sf")] },
-  { configurable: { thread_id: "42" } },
-);
+const nextState = await app.invoke({
+  messages: [...finalState.messages, new HumanMessage("Analyse the values and find the ones that are not normal (maybe too high or too low)")],
+});
+console.log(nextState.messages[nextState.messages.length - 1].content);
 
-console.log(
-  agentFinalState.messages[agentFinalState.messages.length - 1].content,
-);
-
-const agentNextState = await agent.invoke(
-  { messages: [new HumanMessage("what about ny")] },
-  { configurable: { thread_id: "42" } },
-);
-
-console.log(
-  agentNextState.messages[agentNextState.messages.length - 1].content,
-);
+const lastState = await app.invoke({
+  messages: [...finalState.messages, new HumanMessage("Give me recommandation on how to correct those values")],
+});
+console.log(lastState.messages[lastState.messages.length - 1].content);
