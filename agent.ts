9f5c8542edcfa4eea7432c8d4bd8f63dd36b8ea7
Zenati Mehdi <mehdi1.zenati@epitech.eu>
2025-03-22 16:31:51 +0100

    Better output, temporarily removed tools

--- agent.ts
+++ agent.ts
@@ -10,6 +10,12 @@ import { ToolNode } from "@langchain/langgraph/prebuilt";
 import { StateGraph, MessagesAnnotation } from "@langchain/langgraph";
 import RapportJSON from "./assets/rapport.json";
 import GoodRapportJSON from "./assets/goodRapport.json";
+import TresholdJSON from "./assets/treshold.json";
+import readline from "node:readline";
+
+const rapport = JSON.stringify(RapportJSON);
+const goodRapport = JSON.stringify(GoodRapportJSON);
+const treshold = JSON.stringify(TresholdJSON);
 
 const tools = [new TavilySearchResults({ maxResults: 3 })];
 const toolNode = new ToolNode(tools);
@@ -17,14 +23,15 @@ const toolNode = new ToolNode(tools);
 const model = new ChatOpenAI({
   model: "gpt-4o-mini",
   temperature: 0,
-}).bindTools(tools);
+});
+// .bindTools(tools);
 
 function shouldContinue({ messages }: typeof MessagesAnnotation.State) {
   const lastMessage = messages[messages.length - 1] as AIMessage;
 
-  if (lastMessage.tool_calls?.length) {
-    return "tools";
-  }
+  // if (lastMessage.tool_calls?.length) {
+  //   return "tools";
+  // }
   return "__end__";
 }
 
@@ -34,30 +41,63 @@ async function callModel(state: typeof MessagesAnnotation.State) {
   return { messages: [response] };
 }
 async function analyze(state: typeof MessagesAnnotation.State) {
+  console.clear();
+  console.log("Analyzing data...");
   const response = await model.invoke([
     ...state.messages,
     new HumanMessage(
-      "Analyze the provided values and identify only those that are abnormally high or dangerous. If no values exceed a critical threshold or are considered dangerous Do not provide any additional explanation if everything is normal. Respond only with 'R.A.S.'. ignore values that, while potentially high, do not pose a significant risk or anomaly."
+      "Analyze the provided values and compare them against the thresholds object. Identify only those values that exceed the defined thresholds or are considered dangerous. If no values exceed a critical threshold or are considered dangerous, respond only with 'R.A.S.'. Here's the treshold : " +
+        treshold
     ),
   ]);
   return { messages: [response] };
 }
 
-//Todo: Mieux structurer l'output
 async function recommendations(state: typeof MessagesAnnotation.State) {
+  console.clear();
+  console.log("Generating recommendations...");
   const response = await model.invoke([
     ...state.messages,
-    new HumanMessage("Give me recommendations on how to correct those values"),
+    new HumanMessage(
+      "Tell me how many objects were analyzed, how many values per categories were considered dangerous or exceeded the tresholds and give me recommendations on how to correct those values. I want it in this format : The category | number of exceedances | 2 to 3 exemples of the highest values exceeding. Then under this line write a recommendation. Here's an exemple :  Disk Usage | Total Exceedances: 6 instances exceeded 90%. | Highest Values**: 90%, 94%, 96%.' \n - Recommendation: Regularly monitor disk usage and implement data archiving strategies to free up space. Consider upgrading to larger disks or implementing a more efficient storage solution to handle high disk usage. Add at the end,and don't write anything else"
+    ),
   ]);
+  console.clear();
   return { messages: [response] };
+}
+
+async function asktheman(): Promise<string> {
+  return new Promise((resolve) => {
+    console.clear();
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+    rl.question(
+      "No issues found. Everything is normal.\nWould you still like to have recommendations? Y/N: ",
+      (answer) => {
+        answer = answer.toLowerCase();
+        if (answer == "yes" || answer == "y") {
+          resolve("recommendations");
+        } else if (answer == "no" || answer == "n") {
+          resolve("__end__");
+        } else {
+          console.log("Invalid input. Aborting.");
+          resolve("__end__");
+        }
+        rl.close();
+      }
+    );
+    rl.close();
+  });
 }
 
 async function shouldRecommend({ messages }: typeof MessagesAnnotation.State) {
   if (messages[messages.length - 1].content.includes("R.A.S." as any)) {
-    messages[messages.length - 1].content = "No issues found. Everything is normal.";
-    //Todo: ajouter un input user pour savoir s'il veut quand même des recommandations ?
-    return "__end__";
+    const result = (await asktheman()) as string;
+    return result;
   }
+
   return "recommendations";
 }
 
@@ -70,11 +110,10 @@ const workflow = new StateGraph(MessagesAnnotation)
   .addConditionalEdges("analyze", shouldRecommend)
   .addNode("tools", toolNode)
   .addEdge("tools", "agent")
-  .addConditionalEdges("agent", shouldContinue);
+  .addConditionalEdges("agent", shouldContinue)
+  .addEdge("recommendations", "__end__");
 
 const app = workflow.compile();
-const rapport = JSON.stringify(RapportJSON);
-const goodRapport = JSON.stringify(GoodRapportJSON);
 
 const finalState = await app.invoke({
   messages: [new HumanMessage(rapport)],
